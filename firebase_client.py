5b3c3c7e6127574f7b2421312f4fce307770a1d3
praveentechnic <praveentechnic@gmail.com>
2018-06-03 19:13:30 +0530

    Added sample Firebase db and details

---
+++ firebase_client.py
@@ -0,0 +1,21 @@
+import pyrebase
+
+class firebase_client:
+    def __init__(self):        
+        config = {
+        "apiKey": "dfa76d31d18d6b882d5c08bcd084182eb7313851",
+        "authDomain": "electric-cloud.firebaseapp.com",
+        "databaseURL": "https://electric-cloud.firebaseio.com",
+        "storageBucket": "electric-cloud.appspot.com",
+        "serviceAccount": "electric-cloud-firebase-adminsdk-xuxs4-dfa76d31d1.json"
+        }
+        self.firebase = pyrebase.initialize_app(config)
+        self.firebase.auth()
+
+
+    def putvalue(self, child, data):
+        db= self.firebase.database()
+        db.child(child).set(data)
+
+
+
--- read_meter.py
+++ read_meter.py
@@ -1,7 +1,29 @@
 import time
 import pigpio
+import firebase_client
+from datetime import datetime
+
+timediff=datetime.now()
+counter=0
+
+
+# 1200 imp / hour 
+# current wattage usage = 3600/ (time interval * 1200)
+#call back function, that gets triggered every time there is a pulse
 def cbf(gpio, level, tick):
-   print("{:2d}->{} at {}".format(gpio, level, tick))
+    print("{:2d}->{} at {}".format(gpio, level, tick))
+    global timediff
+    delta=datetime.now() - timediff
+    timediff= datetime.now()
+    wattage = 3600/(delta.second * 1200)
+    global counter
+    if(counter>10):
+        fbc= firebase_client.firebase_client()
+        fbc.putvalue("realtime_data","x")
+        print("Updated Realtime usage to Cloud")
+        fbc=None
+        counter=0
+    counter= counter+1
 
 GPIO=15
 GLITCH=500
